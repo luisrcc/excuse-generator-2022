6b89d5f5a991e99259381a3d2a644840c8358508
luisrcc <luiscastillo1991@gmail.com>
2022-12-19 11:30:26 +0000

    ejercicio-excuse-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = parseInt(Math.random() * (who.length + 1 - 1) + 1);
+  let que = parseInt(Math.random() * (what.length + 1 - 1) + 1);
+  let cuando = parseInt(Math.random() * (when.length + 1 - 1) + 1);
+
+  let concat = who[quien - 1] + " " + what[que - 1] + " " + when[cuando - 1];
+
+  return concat;
 };
+
+document.getElementById("excuse").innerText = onload();
